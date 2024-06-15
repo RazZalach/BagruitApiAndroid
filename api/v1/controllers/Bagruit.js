aa3145af075eb21f1c537dcc0b46d70ff9abb686
RazZalach <raz62015@gmail.com>
2024-06-15 16:58:15 +0300

    first commitfdfds

--- api/v1/controllers/Bagruit.js
+++ api/v1/controllers/Bagruit.js
@@ -17,6 +17,96 @@ const Bagruit = [
         "Year": 2023,
         "IsQuetions": true
     },
+    {
+         "NameSubject": "English",
+        "PdfUrl": "https://files.geva.co.il/geva_website/uploads/2024/06/שאלון-16481.pdf",
+        "Year": 2024,
+        "IsQuetions": true
+    },
+    {
+         "NameSubject": "English",
+        "PdfUrl": "https://files.geva.co.il/geva_website/uploads/2024/06/פתרון-16481.pdf",
+        "Year": 2024,
+        "IsQuetions": false
+    },
+    {
+         "NameSubject": "English",
+        "PdfUrl": "https://files.geva.co.il/geva_website/uploads/2023/05/שאלון-16481-קיץ-23.pdf",
+        "Year": 2023,
+        "IsQuetions": true
+    },
+    {
+         "NameSubject": "Hebrew",
+        "PdfUrl": "https://files.geva.co.il/geva_website/uploads/2024/05/שאלון-11281.pdf",
+        "Year": 2024,
+        "IsQuetions": true
+    },
+    {
+         "NameSubject": "Hebrew",
+        "PdfUrl": "https://files.geva.co.il/geva_website/uploads/2024/05/פתרון-11281.pdf",
+        "Year": 2024,
+        "IsQuetions": false
+    },
+    {
+         "NameSubject": "Hebrew",
+        "PdfUrl": "https://files.geva.co.il/geva_website/uploads/2023/05/שאלון-11281-קיץ-23.pdf",
+        "Year": 2023,
+        "IsQuetions": true
+    },
+    {
+         "NameSubject": "Hebrew",
+        "PdfUrl": "https://files.geva.co.il/geva_website/uploads/2023/05/פתרון-11281-קיץ-23-1.pdf",
+        "Year": 2023,
+        "IsQuetions": false
+    },
+    {
+         "NameSubject": "History",
+        "PdfUrl": "https://files.geva.co.il/geva_website/uploads/2024/06/22261-שאלון.pdf",
+        "Year": 2024,
+        "IsQuetions": true
+    },
+    {
+         "NameSubject": "History",
+        "PdfUrl": "https://files.geva.co.il/geva_website/uploads/2024/06/פתרון-22261-קיץ-24.pdf",
+        "Year": 2024,
+        "IsQuetions": false
+    },
+    {
+         "NameSubject": "History",
+        "PdfUrl": "https://files.geva.co.il/geva_website/uploads/2023/06/שאלון-22381-קיץ-2023.pdf",
+        "Year": 2023,
+        "IsQuetions": true
+    },
+    {
+         "NameSubject": "History",
+        "PdfUrl": "https://files.geva.co.il/geva_website/uploads/2023/06/פתרון-22381-קיץ-23.pdf",
+        "Year": 2023,
+        "IsQuetions": false
+    },
+    {
+         "NameSubject": "literature",
+        "PdfUrl": "https://files.geva.co.il/geva_website/uploads/2024/05/שאלון-8381.pdf",
+        "Year": 2024,
+        "IsQuetions": true
+    },
+    {
+         "NameSubject": "literature",
+        "PdfUrl": "https://files.geva.co.il/geva_website/uploads/2024/05/פתרון-8381.pdf",
+        "Year": 2024,
+        "IsQuetions": false
+    },
+    {
+         "NameSubject": "literature",
+        "PdfUrl": "https://files.geva.co.il/geva_website/uploads/2023/05/שאלון-8282-קיץ-23.pdf",
+        "Year": 2023,
+        "IsQuetions": true
+    },
+    {
+         "NameSubject": "literature",
+        "PdfUrl": "https://files.geva.co.il/geva_website/uploads/2023/05/פתרון-לשאלון-8281.pdf",
+        "Year": 2023,
+        "IsQuetions": false
+    },
 ];
 
 
